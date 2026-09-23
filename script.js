22ab483d072426ff30c006f83308e1d9e986f381
Shuvayan Sarkar <shuvayansarkar357@gmail.com>
2026-09-23 22:28:07 +0530

    New Expense Tracker App

---
+++ script.js
@@ -0,0 +1,58 @@
+let navlinks = document.querySelector(".navlinks");
+
+let dashboard = document.querySelector("#dashboard");
+
+let balance = document.querySelector("#balance");
+
+let income = document.querySelector("#income");
+
+let expenses = document.querySelector("#expenses");
+
+let recentlist = document.querySelector("#recent-list");
+
+let transactions = document.querySelector("#transactions");
+
+let addtransactionform = document.querySelector(".add-transaction-form");
+
+let description = document.querySelector("#description");
+
+let amount = document.querySelector("#amount");
+
+let type = document.querySelector("#type");
+
+let category = document.querySelector("#category");
+
+let searchtransaction = document.querySelector(".search-transaction");
+
+let search = document.querySelector("#search");
+
+let filter = document.querySelector(".filter");
+
+let typefilter = document.querySelector("#type-filter");
+
+let categoryfilter = document.querySelector("#category-filter");
+
+let transactionlist = document.querySelector(".transaction-list");
+
+let transactionslist = document.querySelector("#transactions-list");
+
+let analytics = document.querySelector("#analytics");
+
+let totalincome = document.querySelector("#total-income");
+
+let totalexpenses = document.querySelector("#total-expenses");
+
+let totalsavings = document.querySelector("#total-savings");
+
+let categories = document.querySelector("#categories");
+
+let categoryaddition = document.querySelector("#category-addition");
+
+let addcategory = document.querySelector("#add-category");
+
+let settings = document.querySelector("#settings");
+
+let addcurrency = document.querySelector("#add-currency");
+
+let changecurrency = document.querySelector("#change-currency");
+
